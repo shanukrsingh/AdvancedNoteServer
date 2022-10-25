8b3cf6b7dd002e40a515c4f469358396c6004047
shanukrsingh <shanukrsinghhis@gmail.com>
2022-10-25 20:07:39 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+let bodyParser = require('body-parser');
+var cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded(({ extended: true })));
+
+require(`${__dirname}/app/routes`)(app);
+
+app.get('/', (req, res) => {
+    res.send("server is running")
+})
+
+
+app.listen(port, () => {
+    console.log(`server app listening at ${port}`)
+});
